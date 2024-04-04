04d62ef36d7497687e545f476114775b2c4f2e9e
batacraft <lordhunter017@gmail.com>
2024-04-04 15:11:49 +0545

    adding loginpage

---
+++ Src/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../Assets/favicon.ico" type="image/x-icon">
+    <title>MENU MANAGEMENT</title>
+
+    <!--  -->
+    <link rel="stylesheet" href="../../main.css">
+    <link rel="stylesheet" href="../CSS/login_register.css">
+</head>
+<body>
+    
+</body>
+</html>
--- Src/login/login.php
+++ Src/login/login.php
@@ -1,5 +1,10 @@
 
+<?php
+include "../header.php";
 
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,13 +15,39 @@
 
 
     <!-- Custome CSS -->
-    <link rel="stylesheet" href="../../main.css">
-    <title>Document</title>
+    <!-- <link rel="stylesheet" href="../../main.css">
+    <link rel="stylesheet" href="../CSS/login_register.css"> -->
+    <!-- ======================================================= -->
+    <title>Login</title>
 </head>
 <body>
     <div id="warpper">
-        
+        <div id="main">
 
+        
+        <div class="field_group">
+            <form action="#">
+                <h1>Login</h1>
+                <div class="fields">
+                    <input type="text" placeholder="Enter Your Email / Username" value="" name="email" title="email">
+                    <input type="password" placeholder="Enter Your Password" value="" name="password">
+                </div>
+
+               
+
+                <div>
+                    <div class="login_btn">
+                        <button type="submit" name="submit">Login</button>
+                    </div>
+                    <a href="">forgot password?</a>
+                </div>
+
+                <div class="create_account">
+                    <a href="" title="Create an Account">Create an account</a>
+                </div>
+            </form>
+        </div>
+        </div>
     </div>
 </body>
 </html>
